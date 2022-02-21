8d03b19e00d2c0893ee2428a3a7f7a7020acef7e
martincasey1 <98322741+martincasey1@users.noreply.github.com>
2022-02-21 17:11:30 +0000

    Update settings.py

--- web_project/settings.py
+++ web_project/settings.py
@@ -160,8 +160,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_HOST_USER = 'django.blog.reset@gmail.com'
+EMAIL_HOST_PASSWORD = 'zathlhyxztbpisug'
 
 GOOGLE_RECAPTCHA_SECRET_KEY = '6LfQwoYdAAAAABaSWnnKEdtal__xoivgdrgjY1dr'
 SESSION_COOKIE_SECURE = True
