a243099ad12de888fc6cc8a460accad614442af0
martincasey1 <98322741+martincasey1@users.noreply.github.com>
2022-03-10 20:39:04 +0000

    Update settings.py

--- web_project/settings.py
+++ web_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from os import path
 import os
 from pathlib import Path
+import pyodbc
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
