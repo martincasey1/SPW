e634a9277436074d28bc83138b8083483f537327
martincasey1 <98322741+martincasey1@users.noreply.github.com>
2022-03-14 19:26:53 +0000

    Update settings.py

--- web_project/settings.py
+++ web_project/settings.py
@@ -14,8 +14,6 @@ from os import path
 import os
 from pathlib import Path
 import pyodbc
-import django-pyodbc-azure
-import django-pyodbc
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
