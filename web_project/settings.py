46536bfd842481ec1ed5ce867b97fbf450f1dfc0
martincasey1 <98322741+martincasey1@users.noreply.github.com>
2022-03-17 19:34:09 +0000

    Update settings.py

--- web_project/settings.py
+++ web_project/settings.py
@@ -159,7 +159,7 @@ MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
